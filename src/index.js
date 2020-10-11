047b92c6a3a0d7918aaa1fa18e8f2867b4e897e2
Kaiyang Li <gamedevleo@gmail.com>
2020-10-11 17:30:09 +1100

    state in react

--- src/index.js
+++ src/index.js
@@ -140,32 +140,71 @@ let bookList = [
   {"title": "Cat's Cradle","author":"Kurt Vonnegut","pages":304}
 ]
 
-const Book = ({title,author,pages})=>{
+const Book = ({title,author,pages,freeBookmark})=>{
   return (
     <section>
       <h2>{title}</h2>
       <p>by:{author}</p>
       <p>Pages:{pages} pages</p>
+      <p>Free Bookmark: {freeBookmark ? "Yes":"No"}</p>
     </section>
   )
 }
 
-const Library = ({books}) =>{
-  return (
+const Hiring = () =>
     <div>
-      {books.map(
-        (book,i) =>
-          <Book
-            key = {i}
-            title={book.title}
-            author={book.author}
-            pages={book.pages}
-          />
-      )}
+      <p>The library is hiring. Please visit www.library.com/jobs for more</p>
     </div>
-  )
-}
 
+const NotHiring =() =>
+    <div>
+      <p>The library is not hiring. Please come back late for more info.</p>
+    </div>
+
+
+
+class Library extends React.Component{
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     open:false
+  //   }
+  //   this.toggleOpenClosed = this.toggleOpenClosed.bind(this);
+  // }
+  state ={
+    open:true,
+    freeBookmark:true,
+    hiring:true
+  }
+
+  toggleOpenClosed = ()=> {
+    this.setState({
+      open : !this.state.open
+    })
+  }
+
+  render(){
+    const { books } = this.props
+    return (
+      <div>
+        {this.state.hiring ? <Hiring /> : <NotHiring />}
+        <h1>The library is {this.state.open ? "open":"closed"}</h1>
+        <button onClick = {this.toggleOpenClosed}>Toggle</button>
+        {books.map(
+          (book,i) =>
+            <Book
+              key = {i}
+              title={book.title}
+              author={book.author}
+              pages={book.pages}
+              freeBookmark={this.state.freeBookmark}
+            />
+        )}
+
+      </div>
+    )
+  }
+}
 ReactDOM.render(
   <Library books={bookList}/>,
   document.getElementById("root")
